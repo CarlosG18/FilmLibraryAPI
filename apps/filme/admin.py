aa70e4fbe3f2f68420a2a14e8adb9545ef32c28c
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-09-11 10:53:09 -0300

    feat: add modelos Filme, categoria e usuarios no admin site

--- apps/filme/admin.py
+++ apps/filme/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Filme, Categoria
 
 # Register your models here.
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ['nome','ano']
+    list_filter = ['categoria',]
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome',]
